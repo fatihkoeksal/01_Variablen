08dc6be7947c01fd530d2b011a3615fa85eeb049
fatihkoeksal <fthkxl@gmail.com>
2021-07-28 12:08:27 +0200

    Wertzuweisung

--- script_01.js
+++ script_01.js
@@ -1,2 +1,18 @@
 /***** Variablen 01 *******/
 
+//Das ist ein K0mmentar
+
+//Ausgabe
+
+// console.log("H3110 W0r1d!");
+// console.log(firstName);
+
+// Deklaration + Wertzuweisung
+
+let firstName; //Dekleration (Definition)
+firstName = "Max"; //Wertzuweisung
+console.log(firstName); // Ausgabe
+
+let familyName = "Mütze" // Dekleration + Wertzuweisung
+console.log(familyName); // Ausgabe
+console.log(firstName + familyName); // Ausgabe
